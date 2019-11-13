ad6a9af41ba2869cf1ad7e4f2a0d4d85df9d1196
Justin Phillips <justinphil02@yahoo.com>
2019-11-13 01:30:48 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,4 +6,6 @@ html = open("https://flatironschool.com/")
 doc = Nokogiri::HTML(open(html))
 change_careers_with_confidence = doc.css(".title-3Kf9MY")
 puts change_careers_with_confidence
+
+course_titles = doc.css(".title-oE5vT4")
 binding.pry
