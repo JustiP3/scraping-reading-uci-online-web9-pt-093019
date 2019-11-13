d8f38312b1623311f4dc93b2c4fa34e8a5b965ba
Justin Phillips <justinphil02@yahoo.com>
2019-11-13 01:34:54 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -8,4 +8,8 @@ change_careers_with_confidence = doc.css(".title-3Kf9MY")
 puts change_careers_with_confidence
 
 course_titles = doc.css(".title-oE5vT4")
+course1 = course_titles[0]
+course2 = course_titles[1]
+course3 = course_titles[2]
+course4 = course_titles[3]
 binding.pry
